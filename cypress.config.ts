195637cfc1951743495ac59c40e1656206bb0c55
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-10-24 21:18:28 -0400

    replace localhost with ip address

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,9 +12,9 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://127.0.0.1:3000'
   },
 });
