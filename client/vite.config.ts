176f0f696b03328234e3ac8ac35a1344fc446e76
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-10-24 21:08:13 -0400

    test 4

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/_tests_/setup.ts'
-  },
   server: {
     port: 3000,
     host: '127.0.0.1',
@@ -17,8 +12,8 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3001',
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: false,
+      },
+    },
+  },
+});
